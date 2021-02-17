14651bd07d6f4a02ab467ef8d0989beb60575886
Daniel Cordero <web@applehq.eu>
2021-02-17 23:13:56 +0000

    livestream: switch video player to hls.js
    timing/bufferng issues with dash.js, and no dvr

--- js/livestream.js
+++ js/livestream.js
@@ -2,7 +2,7 @@ var livestream = function(uid) {
 	'use strict';
 
 	var header = document.querySelector("header");
-	fetch("https://0xdc.io/dash/" + uid + "/index.mpd")
+	fetch("https://0xdc.io/hls/" + uid + "/index.m3u8")
 	.then(response => {
 		if (!response.ok) {
 			throw new Error("http response not okay. not doing anything");
@@ -14,9 +14,22 @@ var livestream = function(uid) {
 		video.controls = true;
 		video.muted = true;
 
-		var player = dashjs.MediaPlayer().create();
-		player.initialize(video, url, true);
-		header.prepend(video);
+		if (video.canPlayType("application/vnd.apple.mpegurl")) {
+			// native support
+			// load the video and go
+			video.src = url;
+			header.prepend(video);
+			video.play();
+		} else if (Hls.isSupported()) {
+			// using hls.js
+			var hls = new Hls();
+			hls.loadSource(url);
+			hls.attachMedia(video);
+			hls.on(Hls.Events.MANIFEST_PARSED, () => {
+				header.prepend(video);
+				video.play();
+			})
+		}
 	})
 	.catch(() => {
 		console.log("fetch() returned error. not doing anything...");
